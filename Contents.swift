e002fc2e8fd603ae45e25e12ddd93980821043f9
agumy <nov13sh@gmail.com>
2021-05-19 21:45:29 +0900

    try inout param

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,11 @@
-import UIKit
+func mySwap(_ a: inout Int, _ b: inout Int) {
+    let t = a;
+    a = b;
+    b = t
+}
 
-var str = "Hello, playground"
+var x = 100
+var y = 0
+mySwap(&x, &y)
+
+print(x, y)
